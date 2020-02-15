09fc8e5aa662c40286a43657bdb693e8581b9a6c
Felipe Salas Casasola <felipe.salas@possible.com>
2020-02-15 11:43:07 -0600

    Improving title component.

--- components/Title.vue
+++ components/Title.vue
@@ -4,7 +4,16 @@
       <br><br>
       <div class="text-centered">
         <h1 class="title title--big">Exchange Rate Calculator</h1>
-        This is a simple project using NuxtJs, Buefy, Axios and the ExchangeRate API.
+        This is a simple project to start learning some frameworks.
+        The idea of this calculator was posted <br> by
+        <a href="https://medium.com/@indreklasn" target="__blank">
+          <b-tag type="is-black">Indrek Lasn</b-tag>
+        </a> on medium.com
+      </div>
+      <div class="text-centered mg__top">
+        <a class="button is-info is-small">
+          vist his post
+        </a>
       </div>
     </div>
   </div>
@@ -19,7 +28,7 @@ export default {
 <style lang="scss" scoped>
 .title {
   &--big {
-    font-size: 5vw;
+    font-size: 3vw;
   }
 }
 </style>
