da4c7bb0317880a7faa2e1980d476fcc2fca675e
Felipe Salas Casasola <felipe.salas@possible.com>
2020-02-15 11:15:09 -0600

    Centering columns.

--- components/FieldsSection.vue
+++ components/FieldsSection.vue
@@ -4,7 +4,7 @@
       <title-section />
       <section>
         <div class="container mg__top--50">
-          <div class="columns">
+          <div class="columns  is-centered">
             <div class="column is-4">
               <b-field
                 label="From"
@@ -24,20 +24,20 @@
         </div>
 
         <div class="container mg__top">
-          <div class="columns">
-            <div class="column is-4">
+          <div class="columns is-centered">
+            <div class="column is-8">
               <button class="button is-black">swap</button>
             </div>
           </div>
         </div>
 
         <div class="container mg__bt--20">
-          <div class="columns">
+          <div class="columns  is-centered">
             <div class="column is-4">
               <b-field
                 label="To"
                 type="is-black">
-                <b-select placeholder="Select a character" expanded icon="cash">
+                <b-select placeholder="Select another currency" expanded icon="cash">
                   <option value="flint">Flint</option>
                   <option value="silver">Silver</option>
                 </b-select>
@@ -48,7 +48,7 @@
                 label="Result"
                 type="is-black text-centered">
                   <b-input
-                    value="0000"
+                    value="0"
                   >
                   </b-input>
               </b-field>
