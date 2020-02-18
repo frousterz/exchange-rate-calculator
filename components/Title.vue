721b4e3f01b2dd7b1795b59a07bdd256e058f9ce
Felipe Salas Casasola <felipe.salas@possible.com>
2020-02-18 12:22:30 -0600

    Improve text and add missing links.

--- components/Title.vue
+++ components/Title.vue
@@ -4,14 +4,14 @@
       <br><br>
       <div class="text-centered">
         <h1 class="title title--big">Exchange Rate Calculator</h1>
-        This is a simple project to start learning some frameworks.
-        The idea of this calculator was posted <br> by
+        This is a simple project to learn some concepts while I learn more JavaScript.<br>
+        The idea of this calculator was posted by
         <a href="https://medium.com/@indreklasn" target="__blank">
           <b-tag type="is-black">Indrek Lasn</b-tag>
         </a> on medium.com
       </div>
       <div class="text-centered mg__top">
-        <a class="button is-info is-small">
+        <a class="button is-info is-small" href="https://medium.com/better-programming/heres-a-list-of-app-ideas-you-could-start-coding-today-b5f33f94a42e" target="__blank">
           visit his post
         </a>
       </div>
